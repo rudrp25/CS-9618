ad956e8ab816245c3274370a6c84b7e0d3258953
rudrp25 <rudr.p_cis@gemsed.com>
2025-09-15 14:52:57 +0400

    Add files via upload

--- recursive.py
+++ recursive.py
@@ -15,3 +15,11 @@ def power(base, exp):
     else:
         return (base**exp)
 print(power(2, 3))
+
+# recursive function 2
+def Fibonacci(n):
+    if n==0 or n==1:
+        return n
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
