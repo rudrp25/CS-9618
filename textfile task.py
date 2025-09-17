bce3e7ca4257486a51f44b78c3fafd52a34d2767
rudrp25 <rudr.p_cis@gemsed.com>
2025-09-17 12:39:38 +0400

    Add files via upload

---
+++ textfile task.py
@@ -0,0 +1,17 @@
+hobbies = ["","","","",""]
+count = []
+#initializing hobby array
+hobbies[0] = 'Reading books'
+hobbies[1] = 'Play computer games'
+hobbies[2] = 'Sport'
+hobbies[3] = 'Programming'
+hobbies[4] = 'Watching TV'
+
+for i in range(5):
+    num = int(input('Enter a number: '))
+    for i in range(num):
+        if i == num:
+            
+
+
+print(count)
