9ea98350b7c6e760f3f5e0262201de5b5594ec90
rudrp25 <rudr.p_cis@gemsed.com>
2025-09-17 22:20:04 +0400

    Add files via upload

--- textfile task.py
+++ textfile task.py
@@ -1,17 +1,36 @@
-hobbies = ["","","","",""]
-count = []
-#initializing hobby array
-hobbies[0] = 'Reading books'
-hobbies[1] = 'Play computer games'
-hobbies[2] = 'Sport'
-hobbies[3] = 'Programming'
-hobbies[4] = 'Watching TV'
+# initializing arrays
+hobbies = ["Reading books","Play computer games","Sport","Programming","Watching TV"]
+count = [0,0,0,0,0]
 
+file = open("hobby.txt", "w")
+
+# task starts here
 for i in range(5):
-    num = int(input('Enter a number: '))
-    for i in range(num):
-        if i == num:
-            
+    try:
+        choice = int(input('Enter a number: '))
+    except:
+        print('Error, input is not an integer')
+        break
+    try:
+        count[choice-1] += 1
+    except:
+        print('Error, number inputted has to be between 1-5')
+        break
 
 
-print(count)
+for i in range(len(hobbies)):
+    writeLine = str(count[i]) + ", " + hobbies[i] + "\n"
+    file.write(writeLine)
+
+file.close()
+
+file = open("hobby.txt", "r")
+
+for i in range(len(hobbies)):
+    readLine = file.read()
+    print(readLine)
+
+file.close()
+
+
+
